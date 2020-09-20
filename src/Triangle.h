9cdb9e602fc28e7725829cc5dc6f1dda8f6b6ede
Claudio Moreno <claudio.moreno@upr.edu>
2020-09-20 18:54:02 -0400

    first commit

---
+++ src/Triangle.h
@@ -0,0 +1,32 @@
+class Triangle{
+    
+    private:
+    double side1;
+    double side2;
+    double side3;
+
+    public:
+    //Contructor - Inializador del objeto
+    Triangle(double s1, double s2, double s3){
+        this->side1=s1;
+        this->side2=s2;
+        this->side3=s3;
+    }
+
+    //Getters - Extraer las propiedades de los objetos
+    double getSide1(){return this->side1;}
+    double getSide2(){return this->side2;}
+    double getSide3(){return this->side3;}
+
+    /*Setters - Metodos que cambian las propiedades de los objetos
+      en caso que la clase es mutable*/
+    void setSide(double s1, double s2, double s3){
+        this->side1 = s1;
+        this->side2 = s2;
+        this->side3 = s3;
+    }
+    
+    //Instances methos - Interacciones con los objetos
+    double getArea();
+    
+};
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "math.h"
+#include "Triangle.h"
+
+using namespace std;
+
+/* Funcion para calcular el area de un triangulo (Fórmula de Herón)*/
+// static double getArea(double s1, double s2, double s3)
+// {
+//     double s = (s1 + s2 + s3) / 2.0;
+//     double area = sqrt(s * (s-s1) * (s-s2) * (s-s3));
+//     return area;
+// }
+
+// static bool largeThan(Triangle t1, Triangle t2)
+// {
+    
+// }
+
+// static double sumAreas(Triangle triangle[])
+// {
+
+// }
+
+int main(){
+
+    Triangle *t1 = new Triangle(10.0, 10.0, 10.0);
+    Triangle *t2 = new Triangle(20.0, 20.0, 20.0);
+    Triangle *t3 = new Triangle(10.0, 20.0, 29.0);
+   
+    cout << "Area 1: " << t1->getArea() << endl;
+    cout << "Area 2: " << t2->getArea() << endl;
+    cout << "Area 3: " << t3->getArea() << endl;
+
+    return 0;
+}
