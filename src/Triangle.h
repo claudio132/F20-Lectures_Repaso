8e07f246e12eb7459474df0b66110682881124df
Claudio Moreno <claudio.moreno@upr.edu>
2020-09-20 20:12:39 -0400

    Added sumAreas Method to Triangle Class

--- src/Triangle.h
+++ src/Triangle.h
@@ -6,19 +6,19 @@ class Triangle{
     double side3;
 
     public:
-    //Contructor - Inializador del objeto
+    // Contructor - Inializador del objeto
     Triangle(double s1, double s2, double s3){
         this->side1=s1;
         this->side2=s2;
         this->side3=s3;
     }
 
-    //Getters - Extraer las propiedades de los objetos
+    // Getters - Extraer las propiedades de los objetos
     double getSide1(){return this->side1;}
     double getSide2(){return this->side2;}
     double getSide3(){return this->side3;}
 
-    /*Setters - Metodos que cambian las propiedades de los objetos
+    /* Setters - Metodos que cambian las propiedades de los objetos
       en caso que la clase es mutable*/
     void setSide(double s1, double s2, double s3){
         this->side1 = s1;
@@ -26,7 +26,16 @@ class Triangle{
         this->side3 = s3;
     }
     
-    //Instances methos - Interacciones con los objetos
+    // Instances methos - Interacciones con los objetos
     double getArea();
     
+    // Static Methods
+    static double sumAreas(Triangle *triangle[], int numTriangles)
+    {
+        double sum = 0.0;
+        for(int i=0; i < numTriangles; i++){
+            sum += triangle[i]->getArea();
+        }
+        return sum;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -28,10 +28,14 @@ int main(){
     Triangle *t2 = new Triangle(20.0, 20.0, 20.0);
     Triangle *t3 = new Triangle(10.0, 20.0, 29.0);
     
-    cout << "Areas de Tres Triangulos: ";
+    cout << "Areas de Tres Triangulos: " << endl;
     cout << "Area 1: " << t1->getArea() << endl;
     cout << "Area 2: " << t2->getArea() << endl;
     cout << "Area 3: " << t3->getArea() << endl;
+
+    Triangle *triangleList[] = {t1, t2, t3};
+
+    cout << "Area total: " << Triangle::sumAreas(triangleList, 3);
 
     return 0;
 }
